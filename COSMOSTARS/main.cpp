1670b73ead3b8cb01d3ff27f02cbba1cfb316bd0
Vladimir <grom.vas@mail.ru>
2018-12-14 18:30:50 +0300

    TP LESSON

--- COSMOSTARS/main.cpp
+++ COSMOSTARS/main.cpp
@@ -1,6 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
 enum Buff {HP, DEATHLESS, SPEEDUP};
 using namespace sf;
@@ -8,51 +11,90 @@ using namespace sf;
 class Entity{
 protected:	
 	Texture texture;
-public:
 	Sprite sprite;
+
+public:
+	Vector2f position;
 	
 	Entity() {}
 	Entity(std::string path){
 		texture.loadFromFile(path); // загружаем изображение игрока
 		sprite.setTexture(texture);
 	}
+	
+	void setPosition(float x, float y){
+		position.x = x;
+		position.y = y;
+		sprite.setPosition(position);
+	}
 
-	Sprite getSprite() {
+	Sprite getSprite(){
 		return sprite;
 	}
-	void setPosition(float x, float y) {
-		sprite.setPosition(x, y);
+
+	virtual bool move(float) = 0;
+};
+
+class Map:public Entity{
+	float displace;
+public:
+	Map(std::string path):Entity(path){
+		displace = 0;
+	}
+	bool move(float time){
+		if (int(displace += time*1000) >= 960) 
+			displace = 0;
+		setPosition(-displace, 0);
+
+		return false;
 	}
 };
 
 class Asteroid:public Entity{
 	int hp;
+	float speed;
 public:
-	Asteroid(std::string path):Entity(path){}
-	void move(){}
+	Asteroid(std::string path):Entity(path){
+		speed = rand()%10+1;
+		position.x = 900;
+		position.y = rand()%540+0;
+		sprite.setPosition(position);
+	}
+
+	bool move(float time){
+		position.x -= speed*time*500;
+		sprite.setPosition(position);
+
+		return (position.x < -96) ? true : false;
+	}
 };
-
-
 
 class Bonus:public Entity{
 public:
 	Buff buff;
 	Bonus(std::string path, Buff buff):Entity(path), buff(buff){}
-	void move(){}
+	bool move(float time){
+		return false;
+	}
 };
 
 class Player:public Entity{
 	int hp;
 public:
-	Player(std::string path):Entity(path){}
-	void move(){
-		Vector2f position = sprite.getPosition();
+	Player(std::string path):Entity(path){
+		position.x = 0;
+		position.y = 270;
+	}
+	bool move(float time){
 		if (position.y > 0 && Keyboard::isKeyPressed(Keyboard::Up))
-			sprite.move(0, -0.5); 
+			position.y -= 0.5*time*10000;
 		else if (position.y < 444 && Keyboard::isKeyPressed(Keyboard::Down)) 
-			sprite.move(0, 0.5);
+			position.y += 0.5*time*10000;
 		else if (Keyboard::isKeyPressed(Keyboard::Space))
 			this->attack();
+		sprite.setPosition(position);
+
+		return false;
 	}
 
 	void attack(){
@@ -72,37 +114,40 @@ public:
 };
 
 int main(){
+	srand(time(NULL));
 	VideoMode desktop = VideoMode::getDesktopMode();
 	RenderWindow window(VideoMode(960, 540, desktop.bitsPerPixel), "Cosmo");
 
-	Player player("images/ship.png");
-	Entity map("images/cosmos.png");
+	static Entity* arr[] = {new Map("images/cosmos.png"), new Player("images/ship.png")};
+	std::vector<Entity*> entities(arr, arr + sizeof(arr)/sizeof(arr[0]));
+
+	for(int j = 0; j <= rand()%4; j++)
+		entities.push_back(new Asteroid("images/asteroid.png"));
+
 	Clock clock; 
 
-	player.setPosition(0, 270);//
-
-	float i = 0;
-
-	// ќсновной (бесконечный) цикл
 	while (window.isOpen()){
 		Event event; //ѕеременна¤ дл¤ событи¤
 		while (window.pollEvent(event)) //ќпрос событий
 			if (event.type == Event::Closed)
 				window.close();
-
-		float time = clock.getElapsedTime().asSeconds();
-		if (int(i += time*1000) >= 960) 
-			i = 0;
-
-		map.setPosition(-i, 0);
-		player.move();
 		
 		window.clear();
-		window.draw(map.sprite);
-		window.draw(player.sprite);
+
+		float time = clock.getElapsedTime().asSeconds();
+
+		for(std::vector<Entity*>::iterator it = entities.begin(); it != entities.end(); ){
+			window.draw((*it)->getSprite());
+			if ((*it)->move(time)) 
+				entities.erase(it);
+			else
+				++it;
+			std::cout << entities.size();
+		}
+
 		window.display(); 
 		clock.restart();
 	}
-
+	
 	return 0;
 }
