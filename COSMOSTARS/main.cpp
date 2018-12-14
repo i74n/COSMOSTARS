40666993308b980b3d8ace130551d2d5490d70d0
Vladimir <grom.vas@mail.ru>
2018-12-14 03:33:43 +0300

    ADD ENTITIES

--- COSMOSTARS/main.cpp
+++ COSMOSTARS/main.cpp
@@ -1,5 +1,24 @@
 #include <SFML/Graphics.hpp>
 
+class Entity{
+protected:	
+	Texture texture;
+	Sprite sprite;
+public:
+	Entity(std::string path){}
+
+	Sprite getSprite() {}
+	void setPosition(float x, float y) {}
+};
+
+class Player:public Entity{
+	int hp;
+public:
+	Player(std::string path):Entity(path){}
+	void move(){}
+	void attack(){}
+};
+
 int main(){
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
 	sf::RenderWindow window(sf::VideoMode(960, 540, desktop.bitsPerPixel), "Cosmo");
